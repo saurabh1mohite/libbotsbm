a6bb66de21eca478800c00551e0f098d374172e7
saurabh1mohite <mohitesaurabh650@gmail.com>
2022-03-15 12:35:57 -0700

    Initial Commit

---
+++ apps.py
@@ -0,0 +1,221 @@
+from selenium import webdriver
+import os
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import json
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+
+
+
+
+def saurabh_job():
+    op = webdriver.ChromeOptions()
+    op.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    op.add_argument('--headless')
+    op.add_argument('--no-sandbox')
+    op.add_argument('--disable-dev-sh-usage')
+    driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=op)
+    actions = ActionChains(driver)
+
+    link = 'https://asu.libcal.com/reserve/hayden-study'
+    wait = WebDriverWait(driver, 30)
+    try:
+        with open('./saurabh_date.json', 'r') as fp:
+            date = json.load(fp)
+        row = date['row']
+        col = date['col']
+
+        testLink = 'https://asu.libcal.com/reserve/noble'
+        # testLink = 'https://asu.libcal.com/reserve/hayden-study'
+        
+        driver.get(testLink)
+        
+        loadNext = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="eq-time-grid"]/div[1]/div[1]/div/button[2]'
+            ))
+        )
+        loadNext.click()
+        
+        r = 1
+        while True:
+            bookingSlot = wait.until(
+                EC.element_to_be_clickable((
+                    By.XPATH, '//*[@id="eq-time-grid"]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[' + str(r) + ']/td/div/div[2]/div[7]/a'
+                ))
+            )
+            status = bookingSlot.get_attribute('title').strip().split(' - ')
+            if status[-1][0] == 'A':
+                bookingSlot.click()
+                break
+            r += 1
+        submitButton = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="submit_times"]'
+            ))
+        )
+        submitButton.click()
+        continueButton = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="terms_accept"]'
+            ))
+        )
+        continueButton.click()
+
+        submitButton = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="btn-form-submit"]'
+            ))
+        )
+        firstName = driver.find_element_by_id('fname')
+        lastName = driver.find_element_by_id('lname')
+        email = driver.find_element_by_id('email')
+        asuID = driver.find_element_by_id('q1546')
+        
+        firstName.send_keys('Saurabh Balasaheb')
+        lastName.send_keys('Mohite')        
+        email.send_keys('smohite4@asu.edu')
+        asuID.send_keys('1223354340')
+        submitButton.click()
+
+        # After booking set next Date
+
+        if col == 7:
+            if row == 6:
+                row = 1
+                col = 1
+            else:
+                row += 1
+                col= 1
+        else:
+            col += 1
+        date['row'] = row
+        date['col'] = col
+        with open('./saurabh_date.json', 'w') as fp:
+            json.dump(date, fp)
+    finally:
+        try:
+            driver.quit()
+            print(bookingSlot)
+            return True
+        except:
+            return False
+
+def priyanka_job():
+    op = webdriver.ChromeOptions()
+    op.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    op.add_argument('--headless')
+    op.add_argument('--no-sandbox')
+    op.add_argument('--disable-dev-sh-usage')
+    
+    driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=op)
+
+    link = 'https://asu.libcal.com/reserve/hayden-study'
+    wait = WebDriverWait(driver, 10)
+
+    try:
+        with open('./priyanka_date.json', 'r') as fp:
+            date = json.load(fp)
+        row = date['row']
+        col = date['col']
+
+        testLink = 'https://asu.libcal.com/reserve/noble'
+        # testLink = 'https://asu.libcal.com/reserve/hayden-study'
+        driver.get(testLink)
+
+
+        loadNext = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="eq-time-grid"]/div[1]/div[1]/div/button[2]'
+            ))
+        )
+        loadNext.click()
+
+        r = 1
+        while True:
+            bookingSlot = wait.until(
+                EC.element_to_be_clickable((
+                    By.XPATH, '//*[@id="eq-time-grid"]/div[2]/div/table/tbody/tr/td[3]/div/div/div/table/tbody/tr[' + str(r) + ']/td/div/div[2]/div[11]/a'
+                ))
+            )
+            status = bookingSlot.get_attribute('title').strip().split(' - ')
+            if status[-1][0] == 'A':
+                bookingSlot.click()
+                break
+            r += 1
+        submitButton = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="submit_times"]'
+            ))
+        )
+        submitButton.click()
+        continueButton = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="terms_accept"]'
+            ))
+        )
+        continueButton.click()
+
+        submitButton = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH, '//*[@id="btn-form-submit"]'
+            ))
+        )
+        firstName = driver.find_element_by_id('fname')
+        lastName = driver.find_element_by_id('lname')
+        email = driver.find_element_by_id('email')
+        asuID = driver.find_element_by_id('q1546')
+        
+        firstName.send_keys('Priyanka Shivaji')
+        lastName.send_keys('Rothe')        
+        email.send_keys('prothe@asu.edu')
+        asuID.send_keys('1223072877')
+        submitButton.click()
+
+        # After booking set next Date
+
+        if col == 7:
+            if row == 6:
+                row = 1
+                col = 1
+            else:
+                row += 1
+                col= 1
+        else:
+            col += 1
+        date['row'] = row
+        date['col'] = col
+        with open('./priyanka_date.json', 'w') as fp:
+            json.dump(date, fp)
+    finally:
+        try:
+            driver.quit()
+            print(bookingSlot)
+            return True
+        except:
+            return False
+            
+i = 0
+print('SAURABH JOB STARTED')
+result = saurabh_job()
+print('SAURABH JOB-'+str(i) + ' ', result)
+while result == False:
+    i += 1
+    result = saurabh_job()
+    print('SAURABH JOB-'+str(i) + ' ', result)
+print('SAURABH JOB DONE')
+
+
+
+i = 0
+print('PRIYANKA JOB STARTED')
+result = priyanka_job()
+print('PRIYANKA JOB-'+str(i) + ' ', result)
+while result == False:
+    i += 1
+    result = priyanka_job()
+    print('PRIYANKA JOB-'+str(i) + ' ', result)
+print('PRIYANKA JOB DONE')
